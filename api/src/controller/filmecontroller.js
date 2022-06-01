ff8262a05c97f5ee407d18eb73a613d1c558b304
Julio Boaventura <juliospinola.l@gmail.com>
2022-06-01 17:33:17 -0300

    ACABOU O FREI

--- api/src/controller/filmecontroller.js
+++ api/src/controller/filmecontroller.js
@@ -1,4 +1,4 @@
-import {alterarImagem, inserirFilme} from '../repository/filmerepository.js'
+import {alterarImagem, inserirFilme, ListarTodos, ListarId, ListarporNome, removerFilme, alterarFilme} from '../repository/filmerepository.js'
 import multer from 'multer'
 import { Router } from 'express'
 
@@ -66,7 +66,114 @@ server.put('/filme/:id/capa',upload.single('capa'), async (req, resp) => {
     }
 })
 
+server.get('/filme', async (req, resp) => {
+    try{
+        const resposta = await ListarTodos();
+        resp.send(resposta) 
+    }
+    catch(err){
+        resp.status(401).send({
+            erro: err.message
+        })
+    }
+})
 
+server.get('/filme/busca', async(req, resp) => {
+    try{
+    const {nome} = req.query;
+
+    const resposta = await ListarporNome(nome)
+    if(!resposta){
+        resp.status(404).send([])
+    }
+    else{
+    resp.send(resposta)
+}
+    } catch(err){
+        resp.send({
+            erro: err.message
+        })
+    }
+})
+
+server.get('/filme/:id', async(req, resp) => {
+    try{
+    const {id} = req.params;
+
+    const resposta = await ListarId(id)
+    if(!resposta){
+        resp.status(404).send([])
+    }
+    else{
+    resp.send(resposta)
+}
+    } catch(err){
+        resp.send({
+            erro: err.message
+        })
+    }
+})
+
+server.delete('/filme/:id', async (req,resp) =>{
+    try{
+        const {id} = req.params;
+        const resposta = await removerFilme(id)
+
+        if(resposta != 1){
+            throw new Error('Filme não pode ser removido')
+        }
+    resp.status(204).send();
+    } 
+    catch(err){
+        resp.status(404).send({
+            erro: err.message
+        })
+    }
+})
+
+server.put('/filme/:id', async (req, resp) => {
+    try{
+        const {id} = req.params;
+        const filme = req.body;
+
+        if(!filme.nome){
+            throw new Error('Nome do filme é obrigatório!');
+        }
+
+        if(!filme.sinopse){
+            throw new Error('Sinopse do filme é obrigatória!');
+        }
+
+        if(filme.avaliacao == undefined || filme.avaliacao < 0){
+            throw new Error('Avaliação do filme é obrigatória!');
+        }
+
+        if(!filme.lancamento){
+            throw new Error('Data de Lançamento do filme é obrigatória!');
+        }
+
+        if(filme.disponivel){
+            throw new Error('Campo disponível é obrigatório!');
+        }
+
+        if(!filme.usuario){
+            throw new Error('Usuário não logado!');
+        }
+        
+        const resposta = await alterarFilme(id, filme)
+        if(resposta != 1)
+            throw new Error ('Filme não pode ser alterado')
+        
+        else
+            resp.status(204).send()
+    }
+        catch(err){
+            resp.send({
+                erro: err.message
+            })
+        }
+        
+})
 
 export default server;
 
